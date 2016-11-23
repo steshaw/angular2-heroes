c21323ec9d25d049ae8f38041c0da9124cfcd58d
Steven Shaw <steven@steshaw.org>
2016-11-23 21:51:46 +1000

    Extract Heroes component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,11 @@
-import { NgModule }      from '@angular/core'
+import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { FormsModule }   from '@angular/forms'
+import { FormsModule } from '@angular/forms'
 
-import { AppComponent }  from './app.component'
+import { AppComponent } from './app.component'
+import { HeroesComponent } from './heroes.component'
 import { HeroDetailComponent } from './hero-detail.component'
+import { HeroService } from './hero.service'
 
 @NgModule({
   imports: [
@@ -12,8 +14,10 @@ import { HeroDetailComponent } from './hero-detail.component'
   ],
   declarations: [
     AppComponent,
+    HeroesComponent,
     HeroDetailComponent
   ],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
